503eb0e725739c1adc8df7064695c3b51cc24f39
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-03-21 20:49:22 +0300

    animations fixes

--- Assets/Scripts/Fight/RunerFight.cs
+++ Assets/Scripts/Fight/RunerFight.cs
@@ -6,20 +6,34 @@ public class RunerFight: AttackBehavior
 {
     [SerializeField] private Chest _chest;
 
+    private Monster _monsterOfPlayer;
+
     public override void Fight(Monster enemyMonster, Monster monsterOfPlayer)
     {
         Player player = monsterOfPlayer.transform.root.GetComponent<Player>();
-        bool iSPlayerWin = player.Might >= enemyMonster.Level;
+        MonstersAnimatorHandler monstersAnimatorHandler = monsterOfPlayer.transform.root.GetComponent<MonstersAnimatorHandler>();
+        _monsterOfPlayer = monsterOfPlayer;
 
-        if (iSPlayerWin)
+        bool isPlayerWin = player.Might >= enemyMonster.Level;
+        monstersAnimatorHandler.TriggerAttackAnimation();
+
+        if (isPlayerWin)
         {
             player.RaiseMight(enemyMonster.Level);
             enemyMonster.Die();
-            _chest.Push(50f);
+            monsterOfPlayer.SetTarget(enemyMonster);
+            monsterOfPlayer.DealtDamage += Push;
         }
         else
         {
             player.Die();
         }
+    }
+
+    private void Push()
+    {
+        Debug.Log("hi");
+        _chest.Push(50f);
+        _monsterOfPlayer.DealtDamage -= Push;
     }
 }
--- Assets/Scripts/Monsters/Form.cs
+++ Assets/Scripts/Monsters/Form.cs
@@ -5,43 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Form : MonoBehaviour
 {
+    [SerializeField] private Monster _monster;
+
     private Animator _animator;
-    private Monster _monster;
 
     public Animator FormAnimator => _animator;
 
-    private const float _layerMaxWeight = 1f;
-
     private void Awake()
     {
-        _monster = transform.root.GetComponent<Monster>();
         _animator = GetComponent<Animator>();
-    }
-
-    public void ChangeAnimatorLayer(int index)
-    {
-        if (index > 0)
-            StartCoroutine(LayerWeightChange(index));
-    }
-
-    private IEnumerator LayerWeightChange(int index)
-    {
-        float weight = 0.5f;
-        float changeSpeed = _layerMaxWeight / 0.2f;
-
-        while (weight < _layerMaxWeight)
-        {
-            weight = Mathf.MoveTowards(weight, _layerMaxWeight, changeSpeed * Time.deltaTime);
-
-            _animator.SetLayerWeight(index, weight);
-
-            yield return null;
-        } 
     }
 
     private void OnAttack()
     {
-        if(_monster.IsAllive)
-            _monster.GiveDamage();
+        if (_monster.IsAllive)
+        {
+            _monster.DealDamage();
+        }
     }
 }
