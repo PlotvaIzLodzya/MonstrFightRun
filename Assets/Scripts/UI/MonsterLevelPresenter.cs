a5302f752440088178f9a60b3d3be14d532687ef
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-03-22 22:43:04 +0300

    prefabs of monsters

--- Assets/Scripts/UI/MonsterLevelPresenter.cs
+++ Assets/Scripts/UI/MonsterLevelPresenter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-[RequireComponent(typeof(Monster))]
+
 public class MonsterLevelPresenter : LevelPresenter
 {
     [SerializeField] private Canvas _canvas;
