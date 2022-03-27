88b377429c3d3d0f3a3df0b3185e8c1885b0ead8
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-03-27 22:12:59 +0300

    first version

--- Assets/Scripts/PowerUps/MergeAll.cs
+++ Assets/Scripts/PowerUps/MergeAll.cs
@@ -5,17 +5,6 @@ using PathCreation;
 
 public class MergeAll : PowerUp
 {
-    [SerializeField] private PositionAlongPath _heightHandler = new PositionAlongPath();
-
-    private PathCreator _pathCreator;
-
-    private void Start()
-    {
-        _pathCreator = FindObjectOfType<PathCreator>();
-        transform.position = _heightHandler.GetPosition(transform.position, _pathCreator.path);
-        _pathCreator.path.GetClosestPointOnPath(transform.position);
-    }
-
     public override void Use(MonstersHandler monstersHandler)
     {
         monstersHandler.LevelUpAllMonster(1);
